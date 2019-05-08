7d16674905f5504360aceafd3a3050d9383b18b8
Zuzana <centrummandala@gmail.com>
2019-05-08 18:22:52 +0200

    upravy z hodiny

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('Ahoj, jsem startovací projekt!');
+
+if ("geolocation" in navigator) {
+    //geolokace je dostupná a získáme souřadnice:
+    navigator.geolocation.getCurrentPosition(mamLokaci);
+        
+   
+};
+
+function mamLokaci(pozice){
+    console.log(pozice);
+    console.log("AHOJ");
+}
+
